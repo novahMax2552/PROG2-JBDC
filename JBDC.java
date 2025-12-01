2790ec0aa9cefc1cde26031968027dd7868a7228
novahMax2552 <hei.vahatra@gmail.com>
2025-12-01 17:17:27 +0300

    Commit

--- JBDC.java
+++ JBDC.java
@@ -2,3 +2,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
+public class JBDC {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/java-base";
+        String user = "postgres";
+        String password = "Novah Anusha";
