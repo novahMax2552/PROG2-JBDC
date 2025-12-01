6b0d37507f98bb6f88e69a04bc65fdab2802437b
novahMax2552 <hei.vahatra@gmail.com>
2025-12-01 17:18:18 +0300

    Commit

--- JBDC.java
+++ JBDC.java
@@ -8,3 +8,25 @@ public class JBDC {
         String url = "jdbc:postgresql://localhost:5432/java-base";
         String user = "postgres";
         String password = "Novah Anusha";
+try {
+            Class.forName("org.postgresql.Driver");
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connexion réussie !");
+
+            Statement stmt = con.createStatement();
+
+
+            ResultSet rs = stmt.executeQuery("SELECT id, nom FROM etudiants");
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String nom = rs.getString("nom");
+                System.out.println(id + " - " + nom);
+            }
+            con.close();
+            System.out.println("Connexion fermée.");
+
+        } catch (Exception e) {
+            System.out.println("Erreur : " + e.getMessage());
+        }
+    }
+}
